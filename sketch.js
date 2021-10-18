1f3d4b01417ab13b7130c029481cda7981df84fb
arthuregelinas <92753694+arthuregelinas@users.noreply.github.com>
2021-10-18 16:49:32 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,4 @@
+var img = document.createElement("img");
+img.src = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia2.ledevoir.com%2Fimages_galerie%2Fnwd_656429_502733%2Fimage.jpg&f=1&nofb=1";
+var src = document.getElementById("header");
+src.appendChild(img);
