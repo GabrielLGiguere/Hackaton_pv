260a56fb34bfe9ccd2817fac40872d7a45e36457
GabrielLGiguere <86633132+GabrielLGiguere@users.noreply.github.com>
2021-10-19 11:21:17 -0300

    Update box.js

--- box.js
+++ box.js
@@ -1,7 +1,10 @@
-let person = prompt("Comprends-tu qu'il s'agit de pornographie raciste?", "Oui");
+let person = prompt("Comprends-tu qu'il s'agit de pornographie raciste?");
 let text;
-if (person == null || person == "") {
-  text = "User cancelled the prompt.";
-} else {
+if (person == "Oui") {
   text = "Hello " + person + "! How are you today?";
+} else {
+  window.open("https://montreal.ctvnews.ca/mobile/international-women-s-rights-advocates-call-on-canada-to-hold-pornhub-to-account-1.5471518");
+  window.open("https://www.cbc.ca/news/canada/montreal/pornhub-mindgeek-lawsuit-exploitation-1.6070058");
+  window.open("https://https://montreal.ctvnews.ca/pornhub-execs-called-to-testify-in-federal-committee-as-ottawa-looks-at-taking-action-1.5227992");
+
 }
