015efcd915d940a5d5eccbbe10c7acefdea9a2d6
AgustinaBelenP <92754500+AgustinaBelenP@users.noreply.github.com>
2021-10-19 11:08:53 -0400

    Update content_detect_terms.js

--- content_detect_terms.js
+++ content_detect_terms.js
@@ -5,10 +5,21 @@ console.log("Chrome extension go");
 var html = document.getElementsByTagName('html')[0];
 var text = html.innerHTML
 var str = text
-var detect = str.includes("teen")
-var detect = str.includes("bbc")
+const pedo = ["teen", "student","petite","innocent"]
+const agression = ["hole", "slapped","chocked","agressive"]
+const stereotypes = ["bbc", "ebony", "tranny", "lesbian"]
+var detect = str.includes(pedo)
+var detect2 = str.includes(agression)
+var detect3 = str.includes(stereotypes)
+//var detect = str.includes("bbc")
 if (detect=true) {
-  alert('yark')
+  alert('pedofilia')
+}
+if (detect2=true){
+  alert('agression')
+}
+if (detect3=true){
+  alert('stereotypes')
 }
 //for(elt of paragraphs)  {
 //  elt.style['background-color'] = '#FF00FF';
