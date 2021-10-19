7dee743a822e82351ab72f9dbf7a343308b2aca4
GabrielLGiguere <86633132+GabrielLGiguere@users.noreply.github.com>
2021-10-19 12:06:46 -0300

    Update box.js

--- box.js
+++ box.js
@@ -1,7 +1,7 @@
 let person = prompt("Comprends-tu qu'il s'agit de pornographie raciste?");
 let text;
 if (person == "Oui" || person == "oui") {
-  text = "Hello " + person + "! How are you today?";
+   window.location.href = "https://bellesaplus.co";
 } else {
   window.open("https://montreal.ctvnews.ca/mobile/international-women-s-rights-advocates-call-on-canada-to-hold-pornhub-to-account-1.5471518");
   window.open("https://www.cbc.ca/news/canada/montreal/pornhub-mindgeek-lawsuit-exploitation-1.6070058");
